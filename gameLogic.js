d199a78508b20c6c562ef40bc2a2d77d113733e7
streamingturtles.com <pcardillo@streamingturtles.com>
2020-12-03 00:11:09 -0500

    had to revert putting back object into gameLogic.js - broke too many things, need to figure out remaining logic - can always seperate out the obj array to a seperat file later on.

--- gameLogic.js
+++ gameLogic.js
@@ -1,5 +1,36 @@
 
-
+// array to hold key, values for questions.
+var theQuestions = [
+    {   
+        question: 1,
+        question1: "Is Javascript and Java programing related or similiar programming languages?",
+            possibleAnswers1: [
+                "a: Yes",
+                "b: No",
+                "c: Sometimes"
+            ],            
+            correctAnswer1: "b"
+    },
+    {
+        question: 2,
+        question2: "What does a CLOSURE do in Javascript?", 
+            possibleAnswer2: [
+                "a: A Closure gives you access to the outer function's scope from an inner function",
+                "b: A Closure allows you to have private variables",
+                "c: Both b & C"
+            ],
+            correctAnswer2: "c"
+    },
+    {
+        question3: "What is an IIFE in Javascript?", 
+            possibleAnswer2: [
+                "a: Immediately Invoked Function Expression",
+                "b: Instantiated Integer Float Expression",
+                "c: International Integrated Field Electronics"
+            ],
+            correctAnswer3: "a"
+    }
+];
 
 
 
