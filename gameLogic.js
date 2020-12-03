826688ecd24b1fc9ca14e6914769a56da2775cf1
streamingturtles.com <pcardillo@streamingturtles.com>
2020-12-03 00:03:56 -0500

    added quiz questions back to data.js file and modified the object array with identifiers to grab its content in the gameLogic.js file - that's the goal

--- gameLogic.js
+++ gameLogic.js
@@ -2,38 +2,6 @@
 
 
 
-// This is a seperate data.js file to hold all the quiz questions & answers in an array
-
-// an array to hold all the object key-value pairs
-var theQuestions = [
-    {   
-        question1: "Is Javascript and Java programing related or similiar programming languages?",
-            answers1: {
-                a: "Yes",
-                b: "No",
-                c: "Sometimes"
-            },
-            correctAnswer1: "b"
-    },
-    {
-        question2: "What does a CLOSURE do in Javascript?", 
-            answer2: {
-                a: "A Closure gives you access to the outer function's scope from an inner function",
-                b: "A Closure allows you to have private variables",
-                c: "Both b & C"
-            },
-            correctAnswer2: "c"
-    },
-    {
-        question3: "What is an IIFE in Javascript?", 
-            answer2: {
-                a: "Immediately Invoked Function Expression",
-                b: "Instantiated Integer Float Expression",
-                c: "International Integrated Field Electronics"
-            },
-            correctAnswer3: "a"
-    }
-];
 
 
 
